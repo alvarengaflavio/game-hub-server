59097264359be8513526c9e2d7812c07da6c85f5
alvalenda <banysan@msn.com>
2022-11-16 12:16:12 -0300

    feat: implement `swagger` & `class-validator`

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '$/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Server Listening on port 3000');
+
+  // Validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Xbox-Live')
+    .setDescription('The Xbox-Live API Clone')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('user')
+    .addTag('profile')
+    .addTag('game')
+    .addTag('genres')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3333/api
+
+  await app.listen(3333);
 }
 bootstrap();
