cf7f5b77e57abcf5d1be4b9869ec4b9d340f13d0
alvalenda <banysan@msn.com>
2022-11-16 12:28:26 -0300

    fix(status): return string formmat

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n\n Please check http://localhost:3333/api for Swagger documentation.';
+    return 'Server is running! 🚀\n\nPlease check http://localhost:3333/api for Swagger documentation.';
   }
 }
