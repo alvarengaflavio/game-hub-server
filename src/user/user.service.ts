a9a9198cf25537f589f90bfc510d73f2202ca545
alvalenda <banysan@msn.com>
2022-11-25 20:05:54 -0300

    feat(user): implement `update` PATCH route

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,29 @@
 import { PrismaService } from '$/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  private readonly users: User[] = [];
+  private readonly userSelect = {
+    id: true,
+    name: true,
+    email: true,
+  };
+
+  private readonly userDetailedSelect = {
+    ...this.userSelect,
+    cpf: true,
+    isAdmin: true,
+    createdAt: true,
+    updatedAt: true,
+  };
+
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({ select: this.userSelect });
   }
 
   create(dto: CreateUserDto): Promise<User> {
@@ -18,14 +31,31 @@ export class UserService {
       ...dto,
     };
 
-    return this.prisma.user.create({ data });
+    return this.prisma.user.create({ data, select: this.userDetailedSelect });
   }
 
   findOne(id: string): Promise<User> {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: this.userDetailedSelect,
+    });
   }
 
   update(id: string, dto: UpdateUserDto) {
-    throw new Error('Method not implemented.');
+    const user = this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const data: UpdateUserDto = {
+      ...dto,
+    };
+
+    return this.prisma.user.update({
+      where: { id },
+      data,
+      select: this.userDetailedSelect,
+    });
   }
 }
