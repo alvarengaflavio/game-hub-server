0c7747de2258676349613c39721c58788c3ab7c3
alvalenda <banysan@msn.com>
2022-11-30 19:06:03 -0300

    feat(profile): create `response` & `select` dto

--- src/profile/dto/response-profile.dto.ts
+++ src/profile/dto/response-profile.dto.ts
@@ -2,6 +2,8 @@ export class ResponseProfileDto {
   id: string;
   title: string;
   avatarUrl: string;
+  createdAt?: Date;
+  updatedAt?: Date;
   user: {
     id: string;
     name: string;
--- src/profile/dto/select-profile.dto.ts
+++ src/profile/dto/select-profile.dto.ts
@@ -2,6 +2,8 @@ export class SelectProfileDto {
   id: boolean;
   title: boolean;
   avatarUrl: boolean;
+  createdAt: boolean;
+  updatedAt: boolean;
   user: {
     select: {
       id: boolean;
