a476e565e3c380a1de46215bd183ed0df2a0a6d0
alvalenda <banysan@msn.com>
2022-12-01 10:23:25 -0300

    chore(structure): Move `logged-user.decorator` & `exeption` helpers to `common` folder

---
+++ src/common/helpers/prisma-exeption.filter.ts
@@ -0,0 +1,22 @@
+import { PrismaClientValidationError } from '@prisma/client/runtime';
+
+export function prismaExeptionFilter(err: any, message: string): void {
+  if ('code' in err) {
+    if (err.code === 'P2002') {
+      if (err.meta?.target?.includes('email')) {
+        err.name = 'ConflictError';
+        err.message = 'Email já cadastrado';
+      }
+
+      if (err.meta?.target?.includes('cpf')) {
+        err.name = 'ConflictError';
+        err.message = 'CPF já cadastrado';
+      }
+    }
+  }
+
+  if (err instanceof PrismaClientValidationError) {
+    err.name = 'BadRequestError';
+    err.message = message;
+  }
+}
