13b1df87ccad1aa11adb94cc0a83ff18669bc33e
alvalenda <banysan@msn.com>
2022-12-02 14:16:43 -0300

    feat(profile): Set `userId` as optional property

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateProfileDto {
   @IsString()
@@ -17,4 +17,13 @@ export class CreateProfileDto {
       'https://media.discordapp.net/attachments/780200279772626944/1046743073209724928/iYsYcLMR_400x400.jpg',
   })
   avatarUrl: string;
+
+  @IsOptional()
+  @IsUUID()
+  @ApiProperty({
+    description:
+      'O ID do usuário que o perfil pertencerá (somente para admins)',
+    example: 'b4b7c9f7-8d32-47fc-b154-8768a8565e60',
+  })
+  userId?: string;
 }
