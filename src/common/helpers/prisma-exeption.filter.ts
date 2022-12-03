26add924048630a101ef74b3e3a8fb4be4904d35
alvalenda <banysan@msn.com>
2022-12-03 15:36:20 -0300

    feat(exeption): filter error `P2002` name constraint

--- src/common/helpers/prisma-exeption.filter.ts
+++ src/common/helpers/prisma-exeption.filter.ts
@@ -3,14 +3,17 @@ import { PrismaClientValidationError } from '@prisma/client/runtime';
 export function prismaExeptionFilter(err: any, message: string): void {
   if ('code' in err) {
     if (err.code === 'P2002') {
+      err.name = 'ConflictError';
       if (err.meta?.target?.includes('email')) {
-        err.name = 'ConflictError';
         err.message = 'Email já cadastrado';
       }
 
       if (err.meta?.target?.includes('cpf')) {
-        err.name = 'ConflictError';
         err.message = 'CPF já cadastrado';
+      }
+
+      if (err.meta?.target?.includes('name')) {
+        err.message = 'Nome de gênero já cadastrado';
       }
     }
 
