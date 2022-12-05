9e7108f7a8c5276016ab41fdae1bddffaf3edc8b
alvalenda <banysan@msn.com>
2022-12-05 15:15:20 -0300

    feat(favorite): import `PrismaModule` `PassportModule`

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { FavoriteController } from './favorite.controller';
+import { PrismaModule } from '$/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [FavoriteController],
-  providers: [FavoriteService]
+  providers: [FavoriteService],
 })
 export class FavoriteModule {}
