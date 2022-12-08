8be2f9ee0b2042b11f54b1643433f90cb0209284
alvalenda <banysan@msn.com>
2022-12-08 15:52:17 -0300

    fix(homepage): add optional `?` to properties

--- src/homepage/interfaces/response-logged-user.interface.ts
+++ src/homepage/interfaces/response-logged-user.interface.ts
@@ -1,30 +1,30 @@
 export class ResponseLoggedUser {
-  id: string;
-  name: string;
-  email: string;
-  cpf: string;
-  isAdmin: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-  profiles: {
-    id: string;
-    title: string;
-    avatarUrl: string;
-    createdAt: Date;
-    updatedAt: Date;
-    favorites: string[];
+  id?: string;
+  name?: string;
+  email?: string;
+  cpf?: string;
+  isAdmin?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  profiles?: {
+    id?: string;
+    title?: string;
+    avatarUrl?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    favorites?: string[];
   }[];
-  games: {
-    id: string;
-    title: string;
-    genres: string[];
-    year: string;
-    developer: string;
-    score: number;
-    price: number;
-    coverUrl: string;
-    videoUrl: string;
-    createdAt: Date;
-    updatedAt: Date;
+  games?: {
+    id?: string;
+    title?: string;
+    genres?: string[];
+    year?: string;
+    developer?: string;
+    score?: number;
+    price?: number;
+    coverUrl?: string;
+    videoUrl?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
   }[];
 }
