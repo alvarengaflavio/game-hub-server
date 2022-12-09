5c97c019d8a4ac9401240b011c6ae4f44087d17e
alvalenda <banysan@msn.com>
2022-12-08 21:28:41 -0300

    feat(status): change server online string

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n\nPlease check http://localhost:3333/api for Swagger documentation.';
+    return `Server is running! 🚀\n\nPlease check ${
+      process.env.production
+        ? 'https://game-hub.onrender.com/api'
+        : 'http://localhost:3333/api'
+    } for Swagger documentation.`;
   }
 }
